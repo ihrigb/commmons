8b4606045777f49551bbb49ccfcb99297b8862ff
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2020-06-12 05:41:22 -0700

    Install maven wrapper

--- src/main/java/de/ihrigb/commons/Objects.java
+++ src/main/java/de/ihrigb/commons/Objects.java
@@ -1,5 +1,8 @@
 package de.ihrigb.commons;
 
+import java.util.Optional;
+import java.util.function.Function;
+
 /**
  * Java Objects Utils.
  */
@@ -18,6 +21,19 @@ public final class Objects {
 			return true;
 		}
 		return obj0 != null && obj0.equals(obj1);
+	}
+
+	/**
+	 * Maps an object if it is not null.
+	 *
+	 * @param <T>    type of the input value
+	 * @param <R>    type of the return value
+	 * @param value  input value
+	 * @param mapper function to map to R
+	 * @return null, if value is null; otherwise result of the mapper
+	 */
+	public static <T, R> R mapIfNotNull(T value, Function<T, R> mapper) {
+		return Optional.ofNullable(value).map(mapper).orElse(null);
 	}
 
 	private Objects() {
